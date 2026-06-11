5949280de86b02abd535baf8de6e26fae8f0455c
angelitopuntocom <jariveronb18@gmail.com>
2026-06-10 20:30:11 -0600

    "Diseño de vista de registro"

--- app/src/main/java/com/example/examen3/SignUpActivity.kt
+++ app/src/main/java/com/example/examen3/SignUpActivity.kt
@@ -1,26 +1,20 @@
 package com.example.examen3
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        findViewById<Button>(R.id.btnIrRegistro).setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
         }
     }
 }
